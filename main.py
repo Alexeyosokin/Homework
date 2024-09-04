90cf0c8e9a34ccf9c345cc11ba743b2f241df309
Alexeyosokin <alextranssik@gmail.com>
2024-09-04 20:38:09 +0300

    len test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from traceback import print_tb
-
-name = 'alexey'
-res = len(name)//2
-print(name[res:])
-
-
+a = 'Мандарин'
+print(a[0])
+print(a[-1])
+res=len(a)//-3
+print(a[res:])
+print(a[::-1])
+print(a[1:8:2])
