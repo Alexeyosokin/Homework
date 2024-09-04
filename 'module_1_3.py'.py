87a6e02c24330e976db2e2807a8c6313ea837e69
Alexeyosokin <alextranssik@gmail.com>
2024-09-04 21:49:21 +0300

    len test

---
+++ 'module_1_3.py'.py
@@ -0,0 +1,11 @@
+name = 'Alexey'
+print('Name: ' + name)
+age = 50
+print('Age:', age)
+age = age+1
+new_age = 51
+print('New age:', age)
+# is_student= age<age+1
+is_student = new_age == age
+print('Is_student:', is_student)
+
