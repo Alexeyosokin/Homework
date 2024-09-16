6ef6bcb4addda71bf77284c7b60447cf83ec924a
Alexeyosokin <alextranssik@gmail.com>
2024-09-16 22:09:05 +0300

    len test

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-a = 'Мандарин'
-print(a[0])
-print(a[-1])
-res=len(a)//-3
-print(a[res:])
-print(a[::-1])
-print(a[1:8:2])
+def draw_area():
+
+ for i in area:
+    print(*i)
+
+area = [["*", "*", "*"], ["*", "*", "*"], ["*", "*", "*"]]
+print("Добро пожаловать в крестики нолики")
+print("----------------------------------")
+draw_area()
+for turn in range(1, 10):
+    print(f'Ход{turn}')
+    if turn % 2 == "0":
+        print("ходят нолики")
+    else:
+        turn_char = "x"
+        print('{ходят нолики}')
+row = int(input("Введите номер строки(1,2,3)")) - 1
+column = int(input("ведите номер столбца (1,2,3)")) - 1
+
+draw_area()
